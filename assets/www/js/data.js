f9c0ee6a17dd439439aeca3884ea918f72709b40
Andre <an>
2013-02-09 20:42:45 +0000

    database festivals a funcionar, falta todas as outras

--- assets/www/js/data.js
+++ assets/www/js/data.js
@@ -11,12 +11,12 @@ function onDeviceReady() {
 function populateDB(tx) {
     tx.executeSql('DROP TABLE IF EXISTS FESTIVALS');
     tx.executeSql('CREATE TABLE FESTIVALS (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), coordinates VARCHAR(255), city VARCHAR(255), ' +
-        'logo VARCHAR(255), map VARCHAR(255), template VARCHAR(255), updated_at DATETIME)');
+        'logo VARCHAR(255), map VARCHAR(255), template VARCHAR(255), tickets TEXT, transports TEXT, updated_at DATETIME)');
 
     $.getJSON("http://festivall.eu/festivals.json?callback=?", function(data) {
         $.each(data, function(){
-            tx.executeSql('INSERT INTO FESTIVALS (id, name, coordinates, city, logo, map, template, updated_at) VALUES (' + this.id +', "' + this.name + '", "' +
-                this.coord +'", "' + this.city + '", "' + this.logo_url +'", "' + this.map_url + '", "' + this.back_url + '", "' + this.updated_at +'")');
+            tx.executeSql('INSERT INTO FESTIVALS (id, name, coordinates, city, logo, map, template, tickets, transports, updated_at) VALUES (' + this.id +', "' + this.name + '", "' +
+                this.coord +'", "' + this.city + '", "' + this.logo_url +'", "' + this.map_url + '", "' + this.back_url + '", "' +this.tickets + '", "' + this.transports + '" ,"' + this.updated_at +'")');
         });
 
         queryDB(tx);
@@ -32,7 +32,7 @@ function querySuccess(tx, results) {
     var len = results.rows.length;
 
     for (var i=0; i<len; i++){
-        alert(results.rows.item(i).updated_at + " " + results.rows.item(i).map);
+        alert(results.rows.item(i).tickets + " " + results.rows.item(i).transports);
         //console.log("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data);
     }
 }
