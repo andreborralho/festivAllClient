bd903d915da76f2838218a360eaf747a5379bbfa
mp.gomes <mp.gomes89@gmail.com>
2013-04-14 17:14:29 +0100

    feedback and splash
    Signed-off-by: mp.gomes <mp.gomes89@gmail.com>

---
+++ js/feedback.js
@@ -0,0 +1,30 @@
+function createFeedbackContainer(){
+
+    $("#feedbackForm").submit(function(event) {
+
+
+        /* stop form from submitting normally */
+        event.preventDefault();
+
+        /* get some values from elements on the page: */
+        var name = $('#inputFeedbackAuthor').val();
+        var text = $('#inputFeedbackText').val();
+        var email = $('#inputFeedbackAuthorEmail').val();
+
+
+        if(name == "")
+            alert("Please insert a name");
+        else if(text == "")
+            alert("Please insert a comment");
+
+        else{
+            alert('sending : ' + name + ', ' + text + ', ' + email);
+            $.post("http://festivall.eu/feedbacks", {name: name, text: text, email: email}, function(){
+                alert('done');
+                $('#inputFeedbackText').val('');
+            });
+        }
+
+    });
+
+}
